87aba5a3f30a2659b57de115b3b1d2c7e18c8558
houdaspace28 <houdabssd@gmail.com>
2025-12-06 12:41:31 +0100

    skeleton-api

---
+++ api/Controllers/MembersController.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using api.Data;
+using api.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MembersController(AppDbContext context) : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<IReadOnlyList<AppUser>>> GetMembers()
+        {
+          var members=await context.Users.ToListAsync();
+          return members;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetMember(string id)
+        {
+            var member=await context.Users.FindAsync(id);
+            if (member == null) return NotFound();
+            return member;
+        }
+    }
+}
---
+++ api/Data/AppDbContext.cs
@@ -0,0 +1,11 @@
+using System;
+using api.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace api.Data;
+
+public class AppDbContext(DbContextOptions options) : DbContext(options)
+{
+  public DbSet<AppUser> Users { get; set; }
+  
+}
